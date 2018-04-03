924af93c03f56127e16817344c81200ba2984d4b
Rudy Vicario <visotruce@gmail.com>
2018-04-03 11:26:13 +0200

    final

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,7 +1,13 @@
 #include "ros/ros.h"
 #include "lab_ciber_homework1/Student.h"
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char **argv) {
+    char *names[5] = {"Rudy","Paolo","Antonello","Daniele","Giovanni"};
+    char *degree_courses[4] = {"Informatica","Biotecnologia","Scienze politiche","Economia"};
+    srand(time(NULL));
+
     ros::init(argc, argv, "generator");
     ros::NodeHandle n;
 
@@ -11,9 +17,9 @@ int main(int argc, char **argv) {
 
     while(ros::ok()) {
         lab_ciber_homework1::Student msg;
-        msg.name = "Rudy";
-        msg.age = 32;
-        msg.degree_course = "Informatica";
+        msg.name = names[rand()%5];
+        msg.age = rand() % 40 + 18;
+        msg.degree_course = degree_courses[rand()%4];
 
         ROS_INFO("%s - %d - %s\n",msg.name.c_str(),msg.age,msg.degree_course.c_str());
 
